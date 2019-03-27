0796ef2e2be71eb157c6fcd7ef0931f7b9d27540
Daniel1990 <danigd71@gmail.com>
2019-03-27 17:48:49 +0100

    componente ayuda

--- src/app/pages/help/help.page.ts
+++ src/app/pages/help/help.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-help',
-  templateUrl: './help.page.html'
+  templateUrl: './help.page.html',
+  styleUrls: ['help.page.scss']
 })
 export class HelpPage  {
 
